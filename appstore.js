f394f40888d5969ad97844a2881a4d91fa5121c4
Mathias Muller <mathias.muller@4sh.fr>
2015-03-09 11:39:35 +0100

    use playstore

--- appstore.js
+++ appstore.js
@@ -24,17 +24,22 @@ function getOSContent() {
     var getButtonContainer = document.getElementById('downloadButtonContainer');
 
     if(isMobile.iOS()) {
-        button = '<a href="./app/C21_mobile_in_house_app.mobileprovision"> Fichier de sécurité</a> ' +
+        button = //'<a href="./app/C21_mobile_in_house_app.mobileprovision"> Fichier de sécurité</a> ' +
                  '<a href="itms-services://?action=download-manifest&amp;url=https://centurynetrecette.naxos.fr/CnetMobile/app/Info.plist"> ' +
                  '<img src="./img/apple.svg" width="16" height="16"/>' +
                  'Installer l\'application' +
                  '</a> ';
         instructions = 'Pour l\'installation iOS veuillez d\'abord télécharger et installer le fichier de sécurité.'
     } else if(isMobile.Android()) {
-        button = '<a href="./app/C21mobile.apk">' +
-                 '<img src="./img/android.svg" width="16" height="16"/>' +
-                 'Installer l\'application' +
-                 '</a> '
+        button = '<a class="android" href="https://play.google.com/store/apps/details?id=fr.naxos.c21mobile">'+
+                 '<img alt="Télécharger depuis Google Play" '+
+                 ' src="https://developer.android.com/images/brand/fr_generic_rgb_wo_45.png" />'+
+                 '</a>'
+
+//        button = '<a href="./app/C21mobile.apk">' +
+//                 '<img src="./img/android.svg" width="16" height="16"/>' +
+//                 'Installer l\'application' +
+//                 '</a> '
     } else if(isMobile.Windows()) {
         button = '<a href="./app/C21mobile.xap"> ' +
                  '<img src="./img/windows.svg" width="16" height="16"/>' +
