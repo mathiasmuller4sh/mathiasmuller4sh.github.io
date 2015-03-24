c86be1847f42a10a5dab55ffbd57d8ccea3cb822
Mathias Muller <mathias.muller@4sh.fr>
2015-03-24 20:36:18 +0100

    release 0.76.7

--- appstore.js
+++ appstore.js
@@ -22,8 +22,14 @@ function getOSContent() {
     var getDI = document.getElementById('downloadInstructions');
     var getDIContainer = document.getElementById('downloadInstructionsContainer');
     var getButtonContainer = document.getElementById('downloadButtonContainer');
-
-    if(isMobile.iOS()) {
+    if(isMobile.Windows()) {
+        button = '<a href="./app/naxos.cer">Fichier de sécurité</a> ' +
+                 '<a href="./app/C21mobile.xap"> ' +
+                 '<img src="./img/windows.svg" width="16" height="16"/>' +
+                 'Installer l\'application' +
+                 '</a> '
+        instructions = 'Pour l\'installation sur un mobile windows phone, veuillez d\'abord télécharger et installer le fichier de sécurité. (une seule installation suffit.)'
+    }  else  if(isMobile.iOS()) {
         button = //'<a href="./app/C21_mobile_in_house_app.mobileprovision"> Fichier de sécurité</a> ' +
                  '<a href="itms-services://?action=download-manifest&amp;url=https://centurynetrecette.naxos.fr/CnetMobile/app/Info.plist"> ' +
                  '<img src="./img/apple.svg" width="16" height="16"/>' +
