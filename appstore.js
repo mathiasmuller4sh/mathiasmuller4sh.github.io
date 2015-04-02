74442c3442a9ac197638498d8d72eff96d50cd85
Mathias Muller <mathias.muller@4sh.fr>
2015-04-02 15:50:35 +0200

    fix certifcat

--- appstore.js
+++ appstore.js
@@ -23,12 +23,12 @@ function getOSContent() {
     var getDIContainer = document.getElementById('downloadInstructionsContainer');
     var getButtonContainer = document.getElementById('downloadButtonContainer');
     if(isMobile.Windows()) {
-        button = '<a href="./app/naxos.cer">Fichier de sécurité</a> ' +
+        button = '<a href="./app/NAXOS.aetx">Fichier de sécurité</a> ' +
                  '<a href="./app/C21mobile.xap"> ' +
                  '<img src="./img/windows.svg" width="16" height="16"/>' +
                  'Installer l\'application' +
                  '</a> '
-        instructions = 'Pour l\'installation sur un mobile windows phone, veuillez d\'abord télécharger et installer le fichier de sécurité. (une seule installation suffit.)'
+        instructions = 'Pour l\'installation sur un mobile windows phone, veuillez d\'abord télécharger et installer le fichier de sécurité. (une seule installation suffit.)';
     }  else  if(isMobile.iOS()) {
         button = //'<a href="./app/C21_mobile_in_house_app.mobileprovision"> Fichier de sécurité</a> ' +
                  '<a href="itms-services://?action=download-manifest&amp;url=https://centurynetrecette.naxos.fr/CnetMobile/app/Info.plist"> ' +
@@ -41,19 +41,10 @@ function getOSContent() {
 //                 '<img alt="Télécharger depuis Google Play" '+
 //                 ' src="https://developer.android.com/images/brand/fr_generic_rgb_wo_45.png" />'+
 //                 '</a>'
-
         button = '<a href="./app/C21mobile.apk">' +
                  '<img src="./img/android.svg" width="16" height="16"/>' +
                  'Installer l\'application' +
                  '</a> '
-    } else if(isMobile.Windows()) {
-        button = '<a href="./app/NAXOS.aetx">Fichier de sécurité</a> ' +
-                 '<a href="./app/C21mobile.xap"> ' +
-                 '<img src="./img/windows.svg" width="16" height="16"/>' +
-                 'Installer l\'application' +
-                 '</a> '
-        instructions = 'Pour l\'installation sur un mobile windows phone, veuillez d\'abord télécharger et installer le fichier de sécurité. (une seule installation suffit.)'
-
     } else {
         button = '<i>Veuillez-vous connecter avec votre mobile.</i>';
     }
